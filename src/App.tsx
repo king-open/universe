fe356d73cf2623f6052f3f631b4dcbad822f28a1
king-open <kingwang.0624@gmail.com>
2023-10-22 23:48:58 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.scss'
+
+function App() {
+  const [] = useState(0)
+
+  return (
+    <>
+      <div>app</div>
+    </>
+  )
+}
+
+export default App
