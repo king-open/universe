0dca047c393bda6353de3b479d50ec2513b0e57a
king-open <kingwang.0624@gmail.com>
2023-10-26 11:03:00 +0800

    fix: 在unocss后面 加上 true

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -50,21 +50,21 @@ export const WelcomeLayout:React.FC=()=>{
     setHasReadWelcomes(true)
   }
   return (
-    <div className='bg-#5f34bf' h-screen flex flex-col items-stretch pb-16px>
+    <div className="bg-#5f34bf" h-screen="true" flex flex-col="true" items-stretch="true" pb-16px="true">
       <header shrink-0 text-center pt-64px>
         <img src={logo}  w-64px h-69px/>
         <h1 text="#D4D4EE" text-32px>山竹记账</h1>
       </header>
-      <main shrink-1 grow-1  relative ref={main}>
+      <main shrink-1 grow-1="true"  relative ref={main}>
         {transitions((style, pathname) =>
-           <animated.div key={pathname} style={{ ...style, ...extraStyle }} w="100%" h="100%" p-16px flex>
-           <div grow-1 bg-white flex justify-center items-center rounded-8px>
+           <animated.div key={pathname} style={{ ...style, ...extraStyle }} w="100%" h="100%" p-16px="true" flex>
+           <div grow-1="true" bg-white flex justify-center items-center rounded-8px>
              {map.current[pathname]}
            </div>
          </animated.div>
         )}
       </main>
-      <footer shrink-0 text-center text-24px text-white grid grid-cols-3 grid-rows-1>
+      <footer shrink-0 text-center text-24px text-white grid grid-cols-3 grid-rows-1="true">
         <Link style={{ gridArea: '1 / 2 / 2 / 3' }} to={linkMap[location.pathname]}>下一页</Link>
         <Link style={{ gridArea: '1 / 3 / 2 / 4' }}  to="/home" onClick={onSkip}>跳过</Link>
       </footer>
